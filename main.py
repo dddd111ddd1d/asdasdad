16c64cde6424b3d23fcfe7ea72c68ea4ed0485a2
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-11 15:17:18 +0000

    ааа

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ class Person:
   def __init__(self):
     r = requests.get("https://randomuser.me/api/")
     res = r.json()
-    print(res["result"])
+    self.name = res["results"][0]["name"]["first"]
+    self.surname = res["results"][0]["name"]["last"]
+    self.age = res["results"][0]["dob"]["age"]
+
+random_person = Person()
+print(random_person.name)
+print(random_person.surname)
