5fe5c4ebd5aa231789fcef5db4216b6f8947c70c
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-11 15:30:46 +0000

    arab

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 
 class Person:
+  
   def __init__(self):
     r = requests.get("https://randomuser.me/api/")
     res = r.json()
     self.name = res["results"][0]["name"]["first"]
     self.surname = res["results"][0]["name"]["last"]
     self.age = res["results"][0]["dob"]["age"]
+    self.isMale = res["results"][0]["gender"] == "male"
+
 
 random_person = Person()
 print(random_person.name)
 print(random_person.surname)
+print(random_person.age)
+print(random_person.isMale)
+person2 = Person()
